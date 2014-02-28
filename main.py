fa26458bdc8f681be0f1b2ecf9f6b1cbf90b1dea
Samuel Jacob <samueldotj@gmail.com>
2014-02-28 11:11:45 -0800

    Receive and handle notifications in a separate thread.

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ The main program which binds everything else.
 import argparse
 from database import Database
 from rest_client import RestClient
-from notifications import process_notifications
+from notifications import start_notification_thread
 
 verbose_level = 0
 
+
 def process_command_line():
+    """
+    Process command line arguments and assign to appropriate variable
+    """
     global verbose_level
     parser = argparse.ArgumentParser(description='PlugZ Hub.')
     parser.add_argument('--verbose', default=0, help='Increases the logging amount.')
     args = parser.parse_args()
     verbose_level = args.verbose
+
 
 def main():
     process_command_line()
@@ -21,7 +26,8 @@ def main():
     db = Database()
     rc = RestClient(db.hub_id)
     channel_id = rc.connect()
-    process_notifications(channel_id)
+    notification_thread = start_notification_thread(channel_id)
+    notification_thread.join()
 
 
 main()
--- notifications.py
+++ notifications.py
@@ -4,23 +4,37 @@ The notifications may be also a command(turn on light), rule update...
 """
 
 import PubNub
+import threading
 
 
-def process_notifications(channel_id):
+def start_notification_thread(channel_id):
+    """
+    Start notification processing in a separate thread.
+    """
+    notification_thread = threading.Thread(target=_start_notifications, args=(channel_id,))
+    notification_thread.daemon = True
+    notification_thread.start()
+
+    return notification_thread
+
+
+def _start_notifications(channel_id):
     """
     Process push notifications in a loop
     """
+    # TODO - Replace the publish key and subscribe key
     pubnub = PubNub.Pubnub(publish_key='pub-c-9ff29ff2-1427-4864-bbfa-7d3270a233dc',
                            subscribe_key='sub-c-7e20413a-8d2d-11e3-ae86-02ee2ddab7fe',
                            ssl_on=False)
     pubnub.subscribe({
         'channel': channel_id,
-        'callback': notification_received
+        'callback': _notification_received
     })
 
 
-def notification_received(message):
+def _notification_received(message):
     """
     When a notification arrives this function is called to process the message.
     """
+    print 'Notification from pubnub = ' + message
     return True
