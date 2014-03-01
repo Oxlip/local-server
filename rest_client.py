176b43bea24a7836e049713b40dcc0eb7fcd3795
Samuel Jacob <samueldotj@gmail.com>
2014-02-28 18:56:55 -0800

    Made the REST hub API to use identification rather than hub_id. Also added code to make the REST class actually to work.

--- rest_client.py
+++ rest_client.py
@@ -4,40 +4,37 @@ REST client to access the remote server. Provides APIs to access remote server.
 """
 
 import requests
+import plugz_exceptions
 
-REST_SERVER_BASE = 'http://162.243.204.9/backend/api/v1/'
+#REST_SERVER_BASE = 'http://162.243.204.9/backend/api/v1'
+REST_SERVER_BASE = 'http://127.0.0.1:8000/backend/api/v1'
 
 
 class RestClient:
-    def __init__(self, hub_id):
-        self.hub_id = hub_id
-        self.channel_id = None
-
-    def connect(self):
+    def __init__(self, hub_identification):
         """
-        Makes connection to the server and retrieves the channel id for notification.
+        Makes connection to the server and retrieves the hub information (hub_id, channel id for notification).
         """
-        url = '{base}/hub/{hub_id}/connect'.format(base=REST_SERVER_BASE, hub_id=self.hub_id)
+        url = '{base}/hub/{identification}'.format(base=REST_SERVER_BASE, identification=hub_identification)
         r = requests.get(url)
         if r.status_code != requests.codes.ok:
-            return None
+            # TODO - retry before giving up
+            raise plugz_exceptions.ConnectFailedError('REST connection failed {0}'.format(r.status_code))
 
-        json_result = r.json()
-        #todo parse the JSON and get the channel id
-
-        return self.channel_id
+        hub = r.json()
+        self.hub_id = hub['id']
+        self.channel_id = hub['channel_id']
+        self.name = hub['name']
+        self.hub_identification = hub_identification
 
     def get_devices(self, full_refresh=False):
         """
         Makes REST call to server to fetch the device list associated with the user.
         """
-        url = '{base}/hub/{hub_id}/devices'.format(base=REST_SERVER_BASE, hub_id=self.hub_id)
+        url = '{base}/hub/{identification}/devices'.format(base=REST_SERVER_BASE, identification=self.hub_identification)
         r = requests.get(url)
         if r.status_code != requests.codes.ok:
             return None
 
-        json_result = r.json()
-        #todo parse the JSON and return list of devices
-
-        return []
+        return r.json()['devices']
 
