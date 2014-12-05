7d5c925d64facca3fad8cc141fee0f2d7c54f599
Camille Lecuyer <camille.lecuyer@epita.fr>
2014-12-05 20:23:08 +0000

    [SPI] Add ipv6 and RPL configuration.

--- spilowpan.py
+++ spilowpan.py
@@ -17,11 +17,17 @@ def create_lowpan(ip, driver='at86rf230'):
     print 'IP: ' + ip
     cmds = [ 'modprobe ' + driver,
              'iz add wpan-phy0 wpan0 00:00:00:00:00:00:00:01',
-             'iz set wpan0 0x999 0x1 11',
+             'iz set wpan0 0x5449 0x19 11',
              'ip link add link wpan0 name lowpan0 type lowpan',
-             'ip addr add ' + ip + ' dev lowpan0',
+             #'ip addr add ' + ip + ' scope global dev lowpan0',
+             # need a global addr
+             'ip addr add aaaa:0000:0000:0000:00f4:ac6a:0012:4b01 scope global dev lowpan0',
              'ip link set wpan0 up',
-             'ip link set lowpan0 up' ]
+             'ip link set lowpan0 up',
+             'route -A inet6 add ' + ip + '/64 dev lowpan0',
+             'echo 1 > /proc/sys/net/ipv6/conf/all/forwarding',
+             'sysctl -w net.ipv6.conf.lowpan0.rpl_dodag_root=1',
+             'sysctl -w net.ipv6.conf.lowpan0.rpl_enabled=1']
 
     for cmd in cmds:
         os.system(cmd)
